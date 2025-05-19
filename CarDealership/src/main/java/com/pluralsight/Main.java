1c72215f57e5473442b20f235ee1b5fe2fb01a25
KINGZANIFICENT <chefangel.vargas23@gmail.com>
2025-05-19 10:48:25 -0400

    small updates to each script

---
+++ CarDealership/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+
+        UserInterface user = new UserInterface();
+        user.display();
+//        user.processGetAllVehiclesRequest();
+
+        // format outputs to user for better readability
+    }
+}
